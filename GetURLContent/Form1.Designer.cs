5246e0e3d55ba875a3f8c9737883b0ce10de7715
SurgeonVR <gtssvr@mail.ru>
2015-12-14 00:43:59 +0200

    Firs dev version

---
+++ GetURLContent/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace GetURLContent
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbURLList = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.tbContent = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // tbURLList
+            // 
+            this.tbURLList.Location = new System.Drawing.Point(0, 12);
+            this.tbURLList.Name = "tbURLList";
+            this.tbURLList.Size = new System.Drawing.Size(495, 20);
+            this.tbURLList.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(501, 9);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // tbContent
+            // 
+            this.tbContent.Location = new System.Drawing.Point(537, 69);
+            this.tbContent.Multiline = true;
+            this.tbContent.Name = "tbContent";
+            this.tbContent.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tbContent.Size = new System.Drawing.Size(191, 450);
+            this.tbContent.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.textBox1.Location = new System.Drawing.Point(0, 525);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(740, 79);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(0, 49);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(507, 470);
+            this.textBox2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(740, 604);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tbContent);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.tbURLList);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbURLList;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox tbContent;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ GetURLContent/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace GetURLContent
+{
+    public partial class Form1 : Form
+    {
+        public String[] GlobalArray;
+        public Form1()
+        {
+            InitializeComponent();            
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+
+
+            this.MainApp();
+            return;
+
+            ThreadStart ts = new ThreadStart(MainApp);
+            Thread t = new Thread(ts);
+            t.Start();
+            this.textBox1.Lines = this.GlobalArray;
+          // String[] GlobalArray = new String[this.textBox2.Lines.Count()];
+
+            //GenerateURLResource BJ = new GenerateURLResource();
+            // BJ.GenerateURLResourceP(this.textBox2.Lines.ToArray(), this.textBox2);
+            // ThreadStart ts = new ThreadStart(GlobalArray=BJ.MainApp);
+            // Thread t = new Thread(ts);
+            // t.Start();
+
+
+            return;
+
+           System.Net.WebClient  client = new System.Net.WebClient();
+
+            string content = client.DownloadString(this.tbURLList.Text);
+            this.tbContent.Text = content;
+            //this.tbFirstPattern.Text = Properties.Settings.Default.RegExPatternFirst;
+            // foreach (Match match in Regex.Matches(content, Properties.Settings.Default.RegExPatternFirst))
+            // this.textBox1.AppendText(match.Groups[1].Value);
+           //Match match = Regex.Match(content, Properties.Settings.Default.RegExPatternFirst);
+
+
+
+            Regex MyRegexFirst = new Regex(Properties.Settings.Default.RegExPatternFirst, RegexOptions.IgnoreCase);
+            Match match = MyRegexFirst.Match(content);  
+            //this.textBox1.AppendText(match.Value);
+            Regex MyRegexSecond = new Regex(Properties.Settings.Default.RegexPatternSecond, RegexOptions.IgnoreCase);
+            Match match1 = MyRegexSecond.Match(match.ToString());
+            this.textBox1.AppendText(match1.Value + Environment.NewLine) ;
+            //first regex (\$\("#myUniqueImg"\)\.attr\("src",")(\b(?:(?:https?|ftp|file|[A-Za-z]+):\/\/|www\.|ftp\.)(?:\([-A-Z0-9+&@#\/%=~_ |$?!:,.]*\)|[-A-Z0-9+&@#\/%=~_ |$?!:,.])*(?:\([-A-Z0-9+&@#\/%=~_ |$?!:,.]*\)|[A-Z0-9+&@#\/%=~_ |$]))
+            //second regex (\b(?:(?:https?|ftp|file|[A-Za-z]+):\/\/|www\.|ftp\.)(?:\([-A-Z0-9+&@#\/%=~_ |$?!:,.]*\)|[-A-Z0-9+&@#\/%=~_ |$?!:,.])*(?:\([-A-Z0-9+&@#\/%=~_ |$?!:,.]*\)|[A-Z0-9+&@#\/%=~_ |$]))
+
+
+
+
+
+
+
+        }
+
+        private String[] TboxIntoArray()
+        {
+            String[] my_array = new String[this.textBox2.Lines.Count()];
+            my_array = this.textBox2.Lines.ToArray();
+            foreach (string s in my_array)
+                Console.WriteLine(s);
+            return my_array;
+        }
+
+
+
+  
+        private void  MainApp()
+        {
+            System.Net.WebClient client = new System.Net.WebClient();
+
+            String[] my_array = new String[this.textBox2.Lines.Count()];
+            List<string> ContentList = new List<string>();
+
+
+
+            my_array = this.textBox2.Lines.ToArray();
+            foreach (string s in my_array)
+            {
+                string content = client.DownloadString(s);
+                //this.tbContent.Text = content;
+                Regex MyRegexFirst = new Regex(Properties.Settings.Default.RegExPatternFirst, RegexOptions.IgnoreCase);
+                Match match = MyRegexFirst.Match(content);
+                Regex MyRegexSecond = new Regex(Properties.Settings.Default.RegexPatternSecond, RegexOptions.IgnoreCase);
+                Match match1 = MyRegexSecond.Match(match.ToString());
+                this.textBox1.AppendText(match1.Value + Environment.NewLine);
+                ContentList.Add(match1.Value);                
+            }
+            //GlobalArray = ContentList.ToArray();
+            //this.textBox1.Lines = ContentList.ToArray();
+        }
+
+
+
+        public class GenerateURLResource
+        {
+            private String[] InArray;
+            private Object TbLines;
+            public void GenerateURLResourceP(String[] InArray, Object TbLines)
+            {
+                this.InArray = InArray;
+                this.TbLines = TbLines;
+            }
+
+            public String[] MainApp()
+            {
+                System.Net.WebClient client = new System.Net.WebClient();
+
+                String[] my_array = InArray;
+                List<string> ContentList = new List<string>();
+
+
+                my_array = (TbLines as TextBox).Lines.ToArray();
+                foreach (string s in my_array)
+                {
+                    string content = client.DownloadString(s);
+                    //this.tbContent.Text = content;
+                    Regex MyRegexFirst = new Regex(Properties.Settings.Default.RegExPatternFirst, RegexOptions.IgnoreCase);
+                    Match match = MyRegexFirst.Match(content);
+                    Regex MyRegexSecond = new Regex(Properties.Settings.Default.RegexPatternSecond, RegexOptions.IgnoreCase);
+                    Match match1 = MyRegexSecond.Match(match.ToString());
+                    //this.textBox1.AppendText(match1.Value + Environment.NewLine);
+                    ContentList.Add(match1.Value);
+                }
+
+                return ContentList.ToArray();
+                
+            }
+
+        }
+
+
+
+        }
+}
