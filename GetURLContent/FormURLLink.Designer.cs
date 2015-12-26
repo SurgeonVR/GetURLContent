2dc1fb0d968aa52fd0513efa04e06d7801867ce1
SurgeonVR <gtssvr@mail.ru>
2015-12-26 02:53:51 +0200

    next commit 26.12.15

---
+++ GetURLContent/FormURLLink.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace GetURLContent
+{
+    partial class FormURLLink
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormURLLink));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButtonGetSelectedURLs = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripButtonCopyToClipboard = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabelCurrentURL = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripButtonSelectJPG = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.Location = new System.Drawing.Point(0, 25);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBox1.Size = new System.Drawing.Size(412, 469);
+            this.listBox1.TabIndex = 2;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButtonGetSelectedURLs,
+            this.toolStripSeparator1,
+            this.toolStripButtonCopyToClipboard,
+            this.toolStripSeparator2,
+            this.toolStripButtonSelectJPG});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.toolStrip1.Size = new System.Drawing.Size(412, 25);
+            this.toolStrip1.TabIndex = 7;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButtonGetSelectedURLs
+            // 
+            this.toolStripButtonGetSelectedURLs.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButtonGetSelectedURLs.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonGetSelectedURLs.Image")));
+            this.toolStripButtonGetSelectedURLs.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonGetSelectedURLs.Name = "toolStripButtonGetSelectedURLs";
+            this.toolStripButtonGetSelectedURLs.Size = new System.Drawing.Size(93, 22);
+            this.toolStripButtonGetSelectedURLs.Text = "GetSelectedURLs";
+            this.toolStripButtonGetSelectedURLs.Click += new System.EventHandler(this.btnGetSelectedURLs_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // toolStripButtonCopyToClipboard
+            // 
+            this.toolStripButtonCopyToClipboard.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButtonCopyToClipboard.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonCopyToClipboard.Image")));
+            this.toolStripButtonCopyToClipboard.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonCopyToClipboard.Name = "toolStripButtonCopyToClipboard";
+            this.toolStripButtonCopyToClipboard.Size = new System.Drawing.Size(72, 22);
+            this.toolStripButtonCopyToClipboard.Text = "SetClipBoard";
+            this.toolStripButtonCopyToClipboard.Click += new System.EventHandler(this.btnCopyToClipBoard_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 25);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabelCurrentURL});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 494);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(412, 22);
+            this.statusStrip1.TabIndex = 8;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabelCurrentURL
+            // 
+            this.toolStripStatusLabelCurrentURL.Name = "toolStripStatusLabelCurrentURL";
+            this.toolStripStatusLabelCurrentURL.Size = new System.Drawing.Size(0, 17);
+            // 
+            // toolStripButtonSelectJPG
+            // 
+            this.toolStripButtonSelectJPG.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButtonSelectJPG.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonSelectJPG.Image")));
+            this.toolStripButtonSelectJPG.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonSelectJPG.Name = "toolStripButtonSelectJPG";
+            this.toolStripButtonSelectJPG.Size = new System.Drawing.Size(28, 22);
+            this.toolStripButtonSelectJPG.Text = "Jpg";
+            this.toolStripButtonSelectJPG.Click += new System.EventHandler(this.toolStripButtonSelectJPG_Click);
+            // 
+            // FormURLLink
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(412, 516);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "FormURLLink";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FormURLLink";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButtonGetSelectedURLs;
+        private System.Windows.Forms.ToolStripButton toolStripButtonCopyToClipboard;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabelCurrentURL;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripButton toolStripButtonSelectJPG;
+    }
+}
